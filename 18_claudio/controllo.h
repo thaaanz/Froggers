33986812ffbd3cdd3a72a589892ab46fd90e98f5
thaaanz <lauramaria.tani@gmail.com>
2025-01-19 17:31:09 +0100

    Add files via upload

---
+++ 18_claudio/controllo.h
@@ -0,0 +1,19 @@
+#ifndef CONTROLLO_H
+#define CONTROLLO_H
+
+// strttura che rappresenta il rettangolo occupato da ogni oggetto
+typedef struct {
+    int x;
+    int y;
+    int width;
+    int height;
+} BoundingBox;
+
+void controllo(int* pipe_fd, int* pipe_inversa);
+BoundingBox createBoundingBox(int x, int y, int larghezza, int altezza);
+_Bool checksovrapposizione(BoundingBox a, BoundingBox b);
+int checkTaneCollision(int x, int y);
+void handleMorteRana(Processo* rana, int* vite, WINDOW* wgioco);
+_Bool detectCollisione(Processo* rana, Processo* cricca, _Bool* tane, Processo last, WINDOW* wgioco, WINDOW* debug, int* vite, int secondi, int* punti);
+
+#endif
