a1687cf8e7db4adbc17c5c79843b0c9728bd7f7e
thaaanz <lauramaria.tani@gmail.com>
2025-02-04 17:17:15 +0100

    Delete prova_granate_t directory

--- prova_granate_t/campo.h
+++
@@ -1,5 +0,0 @@
-#include "utilities.h"
-void stampaTane( WINDOW * window, _Bool * occupazione);
-void stampaMarciapiede(WINDOW* window);
-void stampaSponda(WINDOW* window);
-void stampaFiume ( WINDOW* window);
