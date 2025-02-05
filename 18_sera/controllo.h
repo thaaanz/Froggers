a075a49e958a83e8f243dd19aaf46c6904317121
thaaanz <lauramaria.tani@gmail.com>
2025-02-05 16:37:51 +0100

    Delete 18_sera directory

--- 18_sera/controllo.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONTROLLO_H
-#define CONTROLLO_H
-
-void controllo(int* pipe_fd, int* pipe_inversa);
-//_Bool detectCollisione (Processo* rana, Processo* cricca, Processo* granate, Processo last, int * vite, WINDOW* debug);
-//_Bool detectCollisione(Processo*rana, Processo *cricca, Processo *granate, Processo last, int *vite,  WINDOW* debug, _Bool * tane , WINDOW* wgico);
-void detectCollisione(Processo* rana, Processo* cricca, _Bool* tane, Processo last, WINDOW* wgioco, WINDOW* debug, int* vite, int secondi, int* punti);
-
-#endif
