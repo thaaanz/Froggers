a2b6e4ef7e4363e50ff2c720cd50e973cf09cfc1
thaaanz <lauramaria.tani@gmail.com>
2025-02-05 16:39:55 +0100

    Delete bozza_tane directory

--- bozza_tane/campo.h
+++
@@ -1,2 +0,0 @@
-void stampaTane( WINDOW * window, _Bool * occupazione);
-void stampaMarciapiede(WINDOW* window);
--- bozza_tane/controllo.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONTROLLO_H
-#define CONTROLLO_H
-
-void controllo(int* pipe_fd, int* pipe_inversa);
-//_Bool detectCollisione (Processo* rana, Processo* cricca, Processo* granate, Processo last, int * vite, WINDOW* debug);
-_Bool detectCollisione(Processo*rana, Processo *cricca, Processo *granate, Processo last, int *vite,  WINDOW* debug, _Bool * tane);
-
-#endif
